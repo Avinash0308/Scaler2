cef86c20b78792e801b7c5dd6b287612f37ed729
Avinash Agrawal <avinash03082003@gmail.com>
2023-09-28 23:48:05 +0530

    Craze of Jawan
    Signed-off-by: Avinash Agrawal <avinash03082003@gmail.com>

---
+++ Avi.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf(" Mai Kaun hoon, mai kya hoon?\n Punya Hoon Ya Paap Hoon?\n Maa se kiya waada hoon, ya adhura koi irada hoon?\n Mai jo hoon, jaise bhi hoon,\n ye khud se puchna tum, kyonki,\n mai bhi aap hoon. hahahaa.....");
+    return 0;
+}
